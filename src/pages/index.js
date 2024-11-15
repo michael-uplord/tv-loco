5f93cb4d22e12542d7b0ae0708fb4e4739811acf
Michael <michael@uplord.co.uk>
2024-11-15 20:04:38 +0000

    add link to show

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import Banner from '../components/Banner';
 import Svg from '../components/Svg';
@@ -35,44 +36,44 @@ export default function Home() {
           <p>No shows were found</p>
           <div className="shows">
             {shows.map((data, index) => (
-            <div key={index} className="show">
-              <div className="image">
-                {data.show.image?.original ? (
-                  <Image
-                    src={data.show.image.original}
-                    alt="Front End Development"
-                    width="500"
-                    height="617"
-                    priority={true}
-                  />
-                ) : (
-                  <Svg name="image-regular" width={32} height={32} />
-                )}
-              </div>
-              <div className="text">
-                <h3>{data.show.name}</h3>
-                <p className="language">Language - English</p>
-                {data.show.rating?.average && (
-                  <p className="rating">
-                    <Svg name="star-solid" width={20} height={20} />
-                    {data.show.rating.average}
+              <Link href={`/show/${data.show.id}`} key={index}>
+                <div className="image">
+                  {data.show.image?.original ? (
+                    <Image
+                      src={data.show.image.original}
+                      alt="Front End Development"
+                      width="500"
+                      height="617"
+                      priority={true}
+                    />
+                  ) : (
+                    <Svg name="image-regular" width={32} height={32} />
+                  )}
+                </div>
+                <div className="text">
+                  <h3>{data.show.name}</h3>
+                  <p className="language">Language - English</p>
+                  {data.show.rating?.average && (
+                    <p className="rating">
+                      <Svg name="star-solid" width={20} height={20} />
+                      {data.show.rating.average}
+                    </p>
+                  )}
+                  <p className="status">
+                    <span
+                      className={`${
+                        data.show.status === "Ended"
+                          ? "error"
+                          : data.show.status === "To Be Determined" || data.show.status === "In Development"
+                          ? "info"
+                          : ""
+                      }`}
+                    >
+                      {data.show.status}
+                    </span>
                   </p>
-                )}
-                <p className="status">
-                  <span
-                    className={`${
-                      data.show.status === "Ended"
-                        ? "error"
-                        : data.show.status === "To Be Determined" || data.show.status === "In Development"
-                        ? "info"
-                        : ""
-                    }`}
-                  >
-                    {data.show.status}
-                  </span>
-                </p>
-              </div>
-            </div>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
